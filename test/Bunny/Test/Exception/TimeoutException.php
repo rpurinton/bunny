f5613a6de52a4585dbfc3a6f774c16c8ae251c30
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-02-17 22:07:36 -0500

    Cleanup

--- test/Bunny/Test/Exception/TimeoutException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace RPurinton\Bunny\Test\Exception;
-
-class TimeoutException extends \RuntimeException
-{
-}
--- test/Library/AbstractClientHelper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace RPurinton\Bunny\Test\Library;
-
-abstract class AbstractClientHelper
-{
-}
